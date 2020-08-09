f32ded478757931a2bee3eca4c57cf669a23dfd6
Ignacio Sanhueza <ignacio.sanhueza.l@gmail.com>
2020-08-08 20:56:03 -0400

    First creation of previousQuestions component

---
+++ front-material/src/Components/Practice/previousQuestions.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const PreviousQuestions = () => {
+  return (
+    <div>
+      <Typography variant="h6">Previous Questions</Typography>
+    </div>
+  );
+};
+
+export default PreviousQuestions;
